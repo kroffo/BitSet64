391d92230ac2c22efa40a0db72533b1f72329582
Kenny Roffo <kroffo@users.noreply.github.com>
2017-01-26 12:43:08 -0500

    Update BitSet.java

--- BitSet/src/bitset/BitSet.java
+++ BitSet/src/bitset/BitSet.java
@@ -12,7 +12,7 @@ public class BitSet {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
+        System.out.println("Good-bye World!");
     }
     
 }
