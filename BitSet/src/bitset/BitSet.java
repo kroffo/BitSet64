0c84508cb8d2f468a0a14d95847adc9f3f56f8be
kennethroffo <kennethroffo@129.3.223.33>
2017-01-26 13:30:50 -0500

    Added Basic BitSet class with add, remove and contains functions. Created display with buttons and text fields.

--- BitSet/src/bitset/BitSet.java
+++ BitSet/src/bitset/BitSet.java
@@ -6,13 +6,23 @@ package bitset;
  * @author kennethroffo
  */
 public class BitSet {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Good-bye World!");
+    private long set;
+    public BitSet() {
+        set = 0;
     }
     
+    public void addNumber(int x) {
+        x = x%64;
+        set |= (1L << x);
+    }   
+    
+    public void removeNumber(int x) {
+        x = x%64;
+        set &= ~(1L << x);
+    }
+    
+    public boolean containsNumber(int x) {
+        x = x%64;
+        return (set & (1L << x)) != 0;
+    }
 }
